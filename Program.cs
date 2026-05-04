730cccd661618d4179083872f375961a3b30b508
LMD <expa_fff@mail.ru>
2026-05-04 17:40:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace semestrovka2
+{
+    struct Measurement
+    {
+        public int Value;
+        public int Operations;
+        public long TimeTicks;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random(42);
+            int[] numbers = new int[10000];
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = rand.Next(1, 1_000_000);
+
+            RedBlackTree tree = new RedBlackTree();
+            List<Measurement> insertMeasurements = new List<Measurement>();
+            List<Measurement> searchMeasurements = new List<Measurement>();
+            List<Measurement> deleteMeasurements = new List<Measurement>();
+
+            Console.WriteLine("Вставка 10000 элементов...");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                tree.ResetOperations();
+                Stopwatch sw = Stopwatch.StartNew();
+                tree.Insert(numbers[i]);
+                sw.Stop();
+                insertMeasurements.Add(new Measurement
+                {
+                    Value = numbers[i],
+                    Operations = tree.OperationCount,
+                    TimeTicks = sw.ElapsedTicks
+                });
+                if ((i + 1) % 1000 == 0)
+                    Console.WriteLine($"  Вставлено {i + 1} элементов");
+            }
+
+            var searchValues = numbers.OrderBy(x => rand.Next()).Take(100).ToList();
+            Console.WriteLine("Поиск 100 случайных элементов...");
+            foreach (int val in searchValues)
+            {
+                tree.ResetOperations();
+                Stopwatch sw = Stopwatch.StartNew();
+                bool found = tree.Find(val);
+                sw.Stop();
+                if (!found)
+                    Console.WriteLine($"Предупреждение: {val} не найден (должен быть)");
+                searchMeasurements.Add(new Measurement
+                {
+                    Value = val,
+                    Operations = tree.OperationCount,
+                    TimeTicks = sw.ElapsedTicks
+                });
+            }
+
+            var deleteValues = numbers.OrderBy(x => rand.Next()).Distinct().Take(1000).ToList();
+            Console.WriteLine("Удаление 1000 случайных элементов...");
+            foreach (int val in deleteValues)
+            {
+                tree.ResetOperations();
+                Stopwatch sw = Stopwatch.StartNew();
+                bool deleted = tree.Delete(val);
+                sw.Stop();
+                if (!deleted)
+                    Console.WriteLine($"Предупреждение: {val} не удалён");
+                deleteMeasurements.Add(new Measurement
+                {
+                    Value = val,
+                    Operations = tree.OperationCount,
+                    TimeTicks = sw.ElapsedTicks
+                });
+            }
+
+            double avgInsertOps = insertMeasurements.Average(m => m.Operations);
+            double avgInsertTimeUs = insertMeasurements.Average(m => m.TimeTicks) / (double)Stopwatch.Frequency * 1_000_000.0;
+            double avgSearchOps = searchMeasurements.Average(m => m.Operations);
+            double avgSearchTimeUs = searchMeasurements.Average(m => m.TimeTicks) / (double)Stopwatch.Frequency * 1_000_000.0;
+            double avgDeleteOps = deleteMeasurements.Average(m => m.Operations);
+            double avgDeleteTimeUs = deleteMeasurements.Average(m => m.TimeTicks) / (double)Stopwatch.Frequency * 1_000_000.0;
+
+            Console.WriteLine("\n========== РЕЗУЛЬТАТЫ ==========");
+            Console.WriteLine($"Вставка:   ср. операций = {avgInsertOps:F2}, ср. время = {avgInsertTimeUs:F3} мкс");
+            Console.WriteLine($"Поиск:     ср. операций = {avgSearchOps:F2}, ср. время = {avgSearchTimeUs:F3} мкс");
+            Console.WriteLine($"Удаление:  ср. операций = {avgDeleteOps:F2}, ср. время = {avgDeleteTimeUs:F3} мкс");
+
+            SaveMeasurementsToCsv("insertions.csv", insertMeasurements);
+            SaveMeasurementsToCsv("searches.csv", searchMeasurements);
+            SaveMeasurementsToCsv("deletions.csv", deleteMeasurements);
+
+            Console.WriteLine("\nИзмерения сохранены в CSV-файлы.");
+        }
+
+        static void SaveMeasurementsToCsv(string fileName, List<Measurement> data)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine("Value,Operations,TimeTicks");
+                foreach (Measurement m in data)
+                    writer.WriteLine($"{m.Value},{m.Operations},{m.TimeTicks}");
+            }
+        }
+    }
+}
---
+++ RedBlackTree.cs
@@ -0,0 +1,398 @@
+﻿using System;
+
+namespace semestrovka2
+{
+    public enum Color { Red, Black }
+
+    public class Node
+    {
+        public int Value;
+        public Color Color;
+        public Node Left, Right, Parent;
+
+        public Node(int value, Color color, Node nil)
+        {
+            Value = value;
+            Color = color;
+            Left = nil;
+            Right = nil;
+            Parent = nil;
+        }
+
+        public Node() { } 
+    }
+
+    public class RedBlackTree
+    {
+        private Node root;
+        private Node nil;
+        public int OperationCount { get; private set; }
+
+        public RedBlackTree()
+        {
+            nil = new Node();
+            nil.Color = Color.Black;
+            nil.Left = nil.Right = nil.Parent = nil;
+            root = nil;
+            OperationCount = 0;
+        }
+
+        public void ResetOperations()
+        {
+            OperationCount = 0;
+        }
+
+        public bool Find(int value)
+        {
+            Node current = root;
+            while (current != nil)
+            {
+                OperationCount++;
+                if (value == current.Value)
+                {
+                    return true;
+                }
+                OperationCount++;
+                if (value < current.Value)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current = current.Right;
+                }
+            }
+            return false;
+        }
+
+        public void Insert(int value)
+        {
+            Node newNode = new Node(value, Color.Red, nil);
+            Node parent = nil;
+            Node current = root;
+
+            while (current != nil)
+            {
+                parent = current;
+                OperationCount++;
+                if (value < current.Value)
+                {
+                    current = current.Left;
+                    OperationCount++;
+                }
+                else if (value > current.Value)
+                {
+                    current = current.Right;
+                    OperationCount++;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            newNode.Parent = parent;
+            if (parent == nil)
+            {
+                root = newNode;
+            }
+            else if (value < parent.Value)
+            {
+                parent.Left = newNode;
+            }
+            else
+            {
+                parent.Right = newNode;
+            }
+
+            FixInsert(newNode);
+        }
+
+        private void FixInsert(Node node)
+        {
+            while (node.Parent.Color == Color.Red)
+            {
+                Node parent = node.Parent;
+                Node grandParent = parent.Parent;
+                if (grandParent == nil) break;
+
+                if (parent == grandParent.Left)
+                {
+                    Node uncle = grandParent.Right;
+                    if (uncle.Color == Color.Red)
+                    {
+                        parent.Color = Color.Black;
+                        uncle.Color = Color.Black;
+                        grandParent.Color = Color.Red;
+                        node = grandParent;
+                        OperationCount += 3;
+                    }
+                    else
+                    {
+                        if (node == parent.Right)
+                        {
+                            node = parent;
+                            RotateLeft(node);
+                            OperationCount++;
+                        }
+                        parent.Color = Color.Black;
+                        grandParent.Color = Color.Red;
+                        RotateRight(grandParent);
+                        OperationCount += 3;
+                    }
+                }
+                else
+                {
+                    Node uncle = grandParent.Left;
+                    if (uncle.Color == Color.Red)
+                    {
+                        parent.Color = Color.Black;
+                        uncle.Color = Color.Black;
+                        grandParent.Color = Color.Red;
+                        node = grandParent;
+                        OperationCount += 3;
+                    }
+                    else
+                    {
+                        if (node == parent.Left)
+                        {
+                            node = parent;
+                            RotateRight(node);
+                            OperationCount++;
+                        }
+                        parent.Color = Color.Black;
+                        grandParent.Color = Color.Red;
+                        RotateLeft(grandParent);
+                        OperationCount += 3;
+                    }
+                }
+            }
+            root.Color = Color.Black;
+            OperationCount++;
+        }
+
+        private void RotateLeft(Node x)
+        {
+            Node y = x.Right;
+            x.Right = y.Left;
+            if (y.Left != nil)
+            {
+                y.Left.Parent = x;
+            }
+            y.Parent = x.Parent;
+            if (x.Parent == nil)
+            {
+                root = y;
+            }
+            else if (x == x.Parent.Left)
+            {
+                x.Parent.Left = y;
+            }
+            else
+            {
+                x.Parent.Right = y;
+            }
+            y.Left = x;
+            x.Parent = y;
+        }
+
+        private void RotateRight(Node x)
+        {
+            Node y = x.Left;
+            x.Left = y.Right;
+            if (y.Right != nil)
+            {
+                y.Right.Parent = x;
+            }
+            y.Parent = x.Parent;
+            if (x.Parent == nil)
+            {
+                root = y;
+            }
+            else if (x == x.Parent.Right)
+            {
+                x.Parent.Right = y;
+            }
+            else
+            {
+                x.Parent.Left = y;
+            }
+            y.Right = x;
+            x.Parent = y;
+        }
+
+        public bool Delete(int value)
+        {
+            Node node = FindNode(value);
+            if (node == nil)
+            { 
+                return false;
+            }
+            DeleteNode(node);
+            return true;
+        }
+
+        private Node FindNode(int value)
+        {
+            Node current = root;
+            while (current != nil)
+            {
+                OperationCount++;
+                if (value == current.Value) return current;
+                OperationCount++;
+                if (value < current.Value) current = current.Left;
+                else current = current.Right;
+            }
+            return nil;
+        }
+
+        private void DeleteNode(Node z)
+        {
+            Node y = z;
+            Node x;
+            Color yOriginalColor = y.Color;
+
+            if (z.Left == nil)
+            {
+                x = z.Right;
+                Transplant(z, z.Right);
+            }
+            else if (z.Right == nil)
+            {
+                x = z.Left;
+                Transplant(z, z.Left);
+            }
+            else
+            {
+                y = Minimum(z.Right);
+                yOriginalColor = y.Color;
+                x = y.Right;
+                if (y.Parent == z)
+                {
+                    x.Parent = y;
+                }
+                else
+                {
+                    Transplant(y, y.Right);
+                    y.Right = z.Right;
+                    y.Right.Parent = y;
+                }
+                Transplant(z, y);
+                y.Left = z.Left;
+                y.Left.Parent = y;
+                y.Color = z.Color;
+                OperationCount++;
+            }
+
+            if (yOriginalColor == Color.Black) 
+            { 
+                FixDelete(x);
+            }
+        }
+
+        private void FixDelete(Node x)
+        {
+            while (x != root && x.Color == Color.Black)
+            {
+                if (x == x.Parent.Left)
+                {
+                    Node w = x.Parent.Right;
+                    if (w.Color == Color.Red)
+                    {
+                        w.Color = Color.Black;
+                        x.Parent.Color = Color.Red;
+                        RotateLeft(x.Parent);
+                        w = x.Parent.Right;
+                        OperationCount += 4;
+                    }
+                    if (w.Left.Color == Color.Black && w.Right.Color == Color.Black)
+                    {
+                        w.Color = Color.Red;
+                        x = x.Parent;
+                        OperationCount++;
+                    }
+                    else
+                    {
+                        if (w.Right.Color == Color.Black)
+                        {
+                            w.Left.Color = Color.Black;
+                            w.Color = Color.Red;
+                            RotateRight(w);
+                            w = x.Parent.Right;
+                            OperationCount += 4;
+                        }
+                        w.Color = x.Parent.Color;
+                        x.Parent.Color = Color.Black;
+                        w.Right.Color = Color.Black;
+                        RotateLeft(x.Parent);
+                        x = root;
+                        OperationCount += 5;
+                    }
+                }
+                else
+                {
+                    Node w = x.Parent.Left;
+                    if (w.Color == Color.Red)
+                    {
+                        w.Color = Color.Black;
+                        x.Parent.Color = Color.Red;
+                        RotateRight(x.Parent);
+                        w = x.Parent.Left;
+                        OperationCount += 4;
+                    }
+                    if (w.Right.Color == Color.Black && w.Left.Color == Color.Black)
+                    {
+                        w.Color = Color.Red;
+                        x = x.Parent;
+                        OperationCount++;
+                    }
+                    else
+                    {
+                        if (w.Left.Color == Color.Black)
+                        {
+                            w.Right.Color = Color.Black;
+                            w.Color = Color.Red;
+                            RotateLeft(w);
+                            w = x.Parent.Left;
+                            OperationCount += 4;
+                        }
+                        w.Color = x.Parent.Color;
+                        x.Parent.Color = Color.Black;
+                        w.Left.Color = Color.Black;
+                        RotateRight(x.Parent);
+                        x = root;
+                        OperationCount += 5;
+                    }
+                }
+            }
+            x.Color = Color.Black;
+            OperationCount++;
+        }
+
+        private void Transplant(Node u, Node v)
+        {
+            if (u.Parent == nil)
+            {
+                root = v;
+            }
+            else if (u == u.Parent.Left)
+            {
+                u.Parent.Left = v;
+            }
+            else
+            {
+                u.Parent.Right = v;
+            }
+            v.Parent = u.Parent;
+        }
+
+        private Node Minimum(Node node)
+        {
+            while (node.Left != nil)
+            {
+                node = node.Left;
+            }
+            return node;
+        }
+    }
+}
